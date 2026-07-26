af50f4e786e8058944813e9720fe8dce11aa9357
houyn655-lang <houyn655@gmail.com>
2026-07-27 01:30:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ rotation_text = "\n".join(
 today_str = today.strftime("%Y-%m-%d")
 
 message_content = (
-    f"⚔️ **[FF14] 오늘의 전장 안내 ({today_str} {weekday_str}요일)** ⚔️\n\n"
-    f"## 오늘의 전장: 🔥 {today_frontline} 🔥\n\n"
+    f"⚔️ ** 오늘의 전장 ({today_str} {weekday_str}요일)** ⚔️\n\n"
+    f"## 오늘은 🔥 {today_frontline} 🔥\n\n"
     f"```text\n"
     f"[ 8일 로테이션 표 ]\n"
     f"{rotation_text}\n"
